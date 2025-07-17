d54ee83f6a0336c79a6a9d2fce1fc0d0ae0ee172
Daniel Fromer <daniel.fromer1@gmail.com>
2025-07-17 12:08:38 +0300

    add the keaderboard to the menu

--- services/gameService.js
+++ services/gameService.js
@@ -1,5 +1,5 @@
 
-import { playGame } from "./playerService.js";
+import { playGame, showLeaderboard } from "./playerService.js";
 import { createRiddle, readRiddles, updateRiddlePrompt, deleteRiddlePrompt } from "./playerService.js";
 
 
@@ -15,6 +15,7 @@ export async function mainMenu() {
         console.log("3. Read all riddles");
         console.log("4. Update an existing riddle");
         console.log("5. Delete a riddle");
+        console.log("6. View leaderboard")
         console.log("0. Exit");
 
         const choice = readline.questionInt("Enter your choice: ");
@@ -25,6 +26,7 @@ export async function mainMenu() {
             case 3: await readRiddles(); break;
             case 4: await updateRiddlePrompt(); break;
             case 5: await deleteRiddlePrompt(); break;
+            case 6: await showLeaderboard();break;
             case 0: console.log(" Bye!"); flag = false; break;
             default: console.log(" Invalid choice.");
         }
